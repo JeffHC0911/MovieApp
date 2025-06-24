6f914d351e8cedf0c85d68bf4f62675803144b15
JeffHC0911 <jefferhc0911@gmail.com>
2025-06-23 23:36:21 -0500

    base view to search for movies (need to add details and improve design)

---
+++ Frontend/MovieApp.WPF/Models/MovieDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieApp.WPF.Models
+{
+    public class MovieDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Year { get; set; } = string.Empty;
+        public string Poster { get; set; } = string.Empty;
+    }
+}
---
+++ Frontend/MovieApp.WPF/Views/SearchView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using MovieApp.WPF.ViewModels;
+
+namespace MovieApp.WPF.Views
+{
+    public partial class SearchView : UserControl
+    {
+        public SearchView()
+        {
+            InitializeComponent();
+            DataContext = new SearchViewModel();
+        }
+    }
+}
